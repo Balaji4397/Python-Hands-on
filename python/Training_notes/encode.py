aafcb798bf0e20a6214a8e45ca955a56104b3fb5
BalajiArunachalam <balaji.a.arunachalam@accenture.com>
2020-10-07 13:47:43 +0530

    Hands-on

--- python/Training_notes/encode.py
+++
@@ -1,6 +0,0 @@
-import pybase64 as b64
-a=b"I am a cricket player"# b is to convert into byte
-c=b64.b64encode(a)
-print(c)
-d=b64.b64decode(c)
-print(d)
--- python/Training_notes/stringmethods.py
+++
@@ -1,8 +0,0 @@
-s="samalehgdyaifgigasdf"
-print(s.count("a",4,11))
-print(s.startswith("s"))
-print(s.endswith("a",2,4))
-#print(s.index("u"))#find can be used for index
-print(s.find("z"))
-f="aBc"
-print(f.replace("b",1,2))
